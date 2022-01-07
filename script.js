ce83b1d2eb588be65558e126cba45bc6577610c0
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-01-07 13:38:59 +0100

    Changed the book object

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 // Global variables
-const author = document.getElementById('author');
-const title = document.getElementById('title');
-const numOfPages = document.getElementById('pages');
-const rating = document.querySelector('.rate');
+
 const myForm = document.querySelector('.myForm');
 const saveButton = document.querySelector('.save');
 const newBtn = document.querySelector('.newb');
@@ -10,28 +7,19 @@ const formDisplay = document.querySelector('.form');
 const card = document.querySelector('.cardContainer');
 const myLibrary = [];
 
-function Book() {
-  this.title = title.value;
-  this.author = author.value;
-  this.numOfPages = numOfPages.value;
-  this.rating = rating.value;
-}
+const book = (author, title, numOfPages, rating) => ({
+  author,
+  title,
+  numOfPages,
+  rating,
+});
 
-// info about the saved book
-Book.prototype.name = function () {
-  return this.author;
-};
-Book.prototype.heading = function () {
-  return this.title;
-};
-Book.prototype.pages = function () {
-  return `${this.numOfPages} pages`;
-};
-Book.prototype.rate = function () {
-  return `Your rating ${this.rating}/10`;
-};
 saveButton.addEventListener('click', (e) => {
   e.preventDefault();
+  const author = document.getElementById('author').value;
+  const title = document.getElementById('title').value;
+  const numOfPages = document.getElementById('pages').value;
+  const rating = document.querySelector('.rate').value;
   const h1 = document.createElement('h3');
   const h2 = document.createElement('h3');
   const h3 = document.createElement('h3');
@@ -42,7 +30,7 @@ saveButton.addEventListener('click', (e) => {
   const bot = document.createElement('div');
   bot.classList.add('bot');
   const x = document.createElement('button');
-  const newBook = new Book();
+  const newBook = book(author, title, numOfPages, rating);
   myLibrary.push(newBook);
   const index = myLibrary.indexOf(newBook);
   console.log(index);
@@ -70,15 +58,13 @@ saveButton.addEventListener('click', (e) => {
     }
   });
   // eslint-disable-next-line array-callback-return
-  myLibrary.filter((book) => {
-    console.log(book);
-
+  myLibrary.filter(() => {
     newCard.classList.add('book');
     newCard.setAttribute('data-id', index);
-    h1.textContent = book.name();
-    h2.textContent = book.heading();
-    h3.textContent = book.pages();
-    h4.textContent = book.rate();
+    h1.textContent = `${newBook.author}`;
+    h2.textContent = `${newBook.title}`;
+    h3.textContent = `${newBook.numOfPages} pages.`;
+    h4.textContent = `Your rating ${newBook.rating}/10`;
     x.textContent = 'Remove';
     x.classList.add('rmvBtn');
     readButton.textContent = 'Have you read it?';
